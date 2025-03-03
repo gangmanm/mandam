9d05ed25dbb8356aa53aea48d5518413ab79a3ee
dklee <dklee7280@gmail.com>
2025-03-03 11:33:22 +0900

    [feat] 썸네일 가져오기 수정

--- src/components/PostComponent.tsx
+++ src/components/PostComponent.tsx
@@ -7,6 +7,8 @@ export default function PostComponent({ title, youtubeUrl, username, id }: { tit
     const [videoId, setVideoId] = useState("");
     const [videoStart, setVideoStart] = useState(0);
     const [thumbnailUrl, setThumbnailUrl] = useState("");
+    const [thumbnailError, setThumbnailError] = useState(false);
+    const defaultThumbnail = "/images/mandam.png";
 
     useEffect(() => {
         console.log(youtubeUrl);
@@ -14,13 +16,13 @@ export default function PostComponent({ title, youtubeUrl, username, id }: { tit
         const videoRealId = videoId.split("&")[0];
         setVideoId(videoRealId);
         console.log(videoRealId);
-        const thumbnailUrl = `https://img.youtube.com/vi/${videoRealId}/maxresdefault.jpg`;
+        const thumbnailUrl = `https://img.youtube.com/vi/${videoRealId}/0.jpg`;
         setThumbnailUrl(thumbnailUrl);
       }, [youtubeUrl]);
 
       
   return <S.PostComponent onClick={() => navigate(`/content/${id}`)}>
-     <S.PostComponentImage src={thumbnailUrl} alt="YouTube Thumbnail" />
+     <S.PostComponentImage src={thumbnailError ? defaultThumbnail : thumbnailUrl} alt="YouTube Thumbnail" onError={() => setThumbnailError(true)} />
      <S.PostInfoContainer>
         <S.PostComponentTitleContainer>
             <S.PostComponentTitle>{title}</S.PostComponentTitle>
