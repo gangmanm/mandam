82aa3e5cbb4c8bbd1442c82e85449075e102b16b
dklee <dklee7280@gmail.com>
2025-03-08 23:09:21 +0900

    [feat] 메뉴바 나오도록 수정

--- src/styles/components/navBar.ts
+++ src/styles/components/navBar.ts
@@ -2,7 +2,6 @@ import { motion } from "framer-motion";
 import styled from "styled-components";
 
 export const Overlay = styled(motion.div)`
-
   position: fixed;
   top: 0;
   left: 0;
@@ -16,6 +15,7 @@ export const NavBarContainer = styled.div`
   width: 100vw;
   height: 50px;
   padding: 15px 20px;
+  z-index: 1000;
 `;
 
 export const NavBarContent = styled.div`
